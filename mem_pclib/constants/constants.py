e6eaad8e2cdc2d20945403bc7852105777f22191
Moyang Wang <mw828@cornell.edu>
2020-03-30 13:38:46 -0400

    nit

--- mem_pclib/constants/constants.py
+++ mem_pclib/constants/constants.py
@@ -9,21 +9,26 @@ Date   : 10 February 2020
 """
 
 from pymtl3 import *
-from pymtl3.stdlib.ifcs.MemMsg       import MemMsgType
+from pymtl3.stdlib.ifcs.MemMsg import MemMsgType
 
-# Constants
+# General
+
 wr = y             = b1(1)
 rd = n = x         = b1(0)
+
+# Write masks
 
 data_array_double_mask = 0xff
 data_array_word_mask   = 0xf
 data_array_2byte_mask  = 0x3
 data_array_byte_mask   = 0x1
 
-READ  = b4(MemMsgType.READ)
-WRITE = b4(MemMsgType.WRITE)
-INIT  = b4(MemMsgType.WRITE_INIT)
-AMO   = b4(3)
+# Shorthands for MemMsgType
+
+READ     = b4(MemMsgType.READ)
+WRITE    = b4(MemMsgType.WRITE)
+INIT     = b4(MemMsgType.WRITE_INIT)
+AMO      = b4(3)
 AMO_ADD  = b4(MemMsgType.AMO_ADD)
 AMO_AND  = b4(MemMsgType.AMO_AND)
 AMO_OR   = b4(MemMsgType.AMO_OR)
@@ -32,4 +37,4 @@ AMO_MIN  = b4(MemMsgType.AMO_MIN)
 AMO_MINU = b4(MemMsgType.AMO_MINU)
 AMO_MAX  = b4(MemMsgType.AMO_MAX)
 AMO_MAXU = b4(MemMsgType.AMO_MAXU)
-AMO_XOR  = b4(MemMsgType.AMO_XOR) 
+AMO_XOR  = b4(MemMsgType.AMO_XOR)
