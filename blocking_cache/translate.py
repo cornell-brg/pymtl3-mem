ed8a611391a0656ea22b438a5b67b7371f48091b
Moyang Wang <mw828@cornell.edu>
2020-04-10 16:17:11 -0400

    add repalce-sram option

--- blocking_cache/translate.py
+++ blocking_cache/translate.py
@@ -11,11 +11,14 @@ Date   : 23 December 2019
 import argparse
 import os
 import sys
+import fileinput
+import subprocess
 
 file_path   = os.path.abspath( __file__ )
 dir_path    = os.path.dirname( file_path )
 parent_path = os.path.dirname( dir_path )
 sys.path.insert( 0, parent_path )
+sram_wrapper_file = os.path.join( parent_path, 'sram', 'brg_gf14_sram_generic.v' )
 
 # Import the translation pass from verilog backend
 from pymtl3.passes.backends.verilog import TranslationConfigs, TranslationPass
@@ -38,15 +41,34 @@ def parse_cmdline():
   p.add_argument( "--abw", default=32, type=int )
   p.add_argument( "--obw", default=8, type=int )
   p.add_argument( "--asso", default=2, type=int )
+  p.add_argument( '--replace-sram', action='store_true', help="Replace SRAM model with real SRAM wrapper" )
   opts = p.parse_args()
   return opts
+
+#=========================================================================
+# Replace SRAM
+#=========================================================================
+
+def replace_sram( file_name ):
+
+  with fileinput.input( file_name, inplace=True ) as f:
+    for line in f:
+      if "SramGenericPRTL__num_bits_128__num_words_256 sram" in line.rstrip('\r\n'):
+        new_line = line.replace( "SramGenericPRTL__num_bits_128__num_words_256 sram",
+                                 "SramGenericPRTL #(.num_bits(128) , .num_words(256)) sram" )
+        print( new_line, end='' )
+      elif "SramGenericPRTL__num_bits_32__num_words_128 sram" in line.rstrip('\r\n'):
+        new_line = line.replace( "SramGenericPRTL__num_bits_32__num_words_128 sram",
+                                 "SramGenericPRTL #(.num_bits(32) , .num_words(128)) sram" )
+        print( new_line, end='' )
+      else:
+        print( line, end='' )
 
 #=========================================================================
 # Runs the translation script
 #=========================================================================
 
-def main():
-  opts = parse_cmdline()
+def main( opts ):
   CacheReqType, CacheRespType = mk_mem_msg(opts.obw, opts.abw, opts.dbw)
   MemReqType, MemRespType = mk_mem_msg(opts.obw, opts.abw, opts.clw)
   # Instantiate the cache
@@ -54,10 +76,12 @@ def main():
                           MemRespType, opts.size, opts.asso )
   success = False
   dut.verilog_translate = True
-  # dut.config_verilog_translate = TranslationConfigs(
-  #     explicit_module_name = 'BlockingCache_{}_{}_{}_{}_{}'.format(opts.size,
-  #     opts.clw, opts.abw, opts.dbw, opts.asso),
-  #   )
+  module_name = f"BlockingCache_{opts.size}_{opts.clw}_{opts.abw}_{opts.dbw}_{opts.asso}"
+  file_name = module_name + ".v"
+  dut.config_verilog_translate = TranslationConfigs(
+      explicit_module_name = module_name,
+      explicit_file_name = file_name
+    )
   try:
     dut.elaborate()
     dut.apply( TranslationPass() )
@@ -67,8 +91,21 @@ def main():
       # path = os.path.join(os.getcwd(), f"{dut.translated_top_module_name}.sv")
       print("\nTranslation finished successfully!")
       # print(f"You can find the generated SystemVerilog file at {path}.")
+      return file_name
     else:
       print("\nTranslation failed!")
+      return None
 
 if __name__ == "__main__":
-  main()
+  opts = parse_cmdline()
+  file_name = main( opts )
+
+  if opts.replace_sram:
+    assert os.path.isfile( sram_wrapper_file ), f"SRAM Wrapper file ({sram_wrapper_file}) doesn't exist"
+    replace_sram( file_name )
+
+    # concat wrapper
+    bashCommand = f"cat {sram_wrapper_file} >> {file_name}"
+    # print( bashCommand )
+    process = subprocess.Popen(bashCommand, stdout=subprocess.PIPE, shell=True)
+    output, error = process.communicate()
---
+++ sram/brg_gf14_sram_generic.v
@@ -0,0 +1,343 @@
+
+module SramGenericPRTL
+ #(parameter num_bits=128 , parameter num_words=256)
+(
+  input logic [($clog2(num_words))-1:0] A1 ,
+  input logic [0:0] CE1 ,
+  input logic [0:0] CSB1 ,
+  input logic [num_bits-1:0] I1 ,
+  output logic [num_bits-1:0] O1 ,
+  input logic [0:0] OEB1 ,
+  input logic [num_bits-1:0] WBM1 ,
+  input logic [0:0] WEB1 ,
+  input logic [0:0] clk ,
+  input logic [0:0] reset 
+);
+
+  localparam logic [31:0] __const__nbytes_at_write_logic  = (num_bits/8);
+ 
+  localparam logic [31:0] __const__num_words_at_write_logic  = num_words;
+  localparam logic [31:0] __const__num_bits_at_write_logic  = num_bits;
+  localparam logic [31:0] __const__num_words_at_update_sram  = num_words;
+
+  wire ceny;
+  wire [num_bits-1:0] weny;
+  wire [$clog2(num_words)-1:0] ay;
+  wire gweny;
+  wire [1:0] so;
+  wire brg_ceny;
+  wire [num_bits-1:0] brg_weny;
+  wire [$clog2(num_words)-1:0] brg_ay;
+  wire brg_gweny;
+  logic [num_bits-1:0] q;
+  wire [1:0] brg_so;
+
+  wire  cen;
+  wire [num_bits-1:0] wen;
+  wire [$clog2(num_words)-1:0] a;
+  wire [num_bits-1:0] d;
+  wire [2:0] ema;
+  wire [1:0] emaw;
+  wire  emas;
+  wire  ten;
+  wire  tcen;
+  wire [num_bits-1:0] twen;
+  wire [$clog2(num_words)-1:0] ta;
+  wire [num_bits-1:0] td;
+  wire  gwen;
+  wire  tgwen;
+  wire  ret1n;
+  wire [1:0] si;
+  wire  se;
+  wire  dftrambyp;
+  wire unused = reset;
+
+
+  assign a = A1;
+  assign d = I1;
+//  assign O1 = q;
+  assign gwen = WEB1;
+  assign cen = CSB1;
+
+  assign dftrambyp = 1'd0;
+  assign ema = 3'd3;
+  assign emas = 1'd0;
+  assign emaw = 2'd1;
+  assign ret1n = 1'd1;
+  assign se = 1'd0;
+  assign si = 2'd0;
+  assign ta = 8'd0;
+  assign tcen = 1'd0;
+  assign td = 128'd0;
+  assign ten = 1'd1;
+  assign tgwen = 1'd0;
+  assign twen = 128'd0;
+
+  assign brg_ceny = ceny;
+  assign brg_weny = weny;
+  assign brg_ay = ay;
+  assign brg_gweny = gweny;
+  assign brg_so = so;
+  genvar j;
+/*
+  for(j = 0; j < __const__nbytes_at_write_logic; j++)
+    assign wen[8*j+:8] = {8{WBM1[j]}};
+*/
+ assign  wen[num_bits-1:0] = WBM1[num_bits-1:0];
+
+  if ((num_bits==128) && (num_words==256)) begin
+ 
+    gf14_sram_256x128_m2 sram_256x128 (.ceny(ceny), .gweny(gweny), .ay(ay), .weny(weny), .q(q), .so(so), .clk(clk), .cen(cen), .gwen(gwen), .a(a), .d(d), .wen(~wen), .stov(1'b0), .ema(ema),
+    .emaw(emaw), .emas(emas), .ten(ten), .tcen(tcen), .tgwen(tgwen), .ta(8'd0), .td(128'd0), .twen(128'd0), .si(si), .se(se), .dftrambyp(dftrambyp), .ret1n(ret1n));
+  end
+  else if ((num_bits==24) && (num_words==256)) begin
+     gf14_sram_256x24_m4 sram_256x24 (.ceny(ceny), .gweny(gweny), .ay(ay), .q(q), .so(so), .clk(clk), .cen(cen), .gwen(gwen), .a(a), .d(d), .stov(1'b0), .ema(ema),
+    .emaw(emaw), .emas(emas), .ten(ten), .tcen(tcen), .tgwen(tgwen), .ta(8'd0), .td(24'd0), .si(si), .se(se), .dftrambyp(dftrambyp), .ret1n(ret1n));
+ 
+  end 
+  else if ((num_bits==32) && (num_words==128)) begin
+    gf14_sram_128x32_m2 sram_128x32 (.ceny(ceny), .gweny(gweny), .ay(ay), .weny(weny), .q(q), .so(so), .clk(clk), .cen(cen), .gwen(gwen), .a(a), .d(d), .wen(~wen), .stov(1'b0), .ema(ema), 
+    .emaw(emaw), .emas(emas), .ten(ten), .tcen(tcen), .tgwen(tgwen), .ta(7'd0), .td(32'd0), .twen(32'd0), .si(si), .se(se), .dftrambyp(dftrambyp), .ret1n(ret1n));
+  end
+  else begin
+  // Do not find a hard SRAM, then use the behavioral model
+  logic [num_bits-1:0] dout_next;
+  logic [num_bits-1:0] ram [0:num_words-1];
+  logic [num_bits-1:0] ram_next [0:num_words-1];
+
+    always_comb begin : read_logic
+      if ( ( !CSB1 ) && WEB1 ) begin
+        dout_next = ram[A1];
+      end
+      else
+        dout_next = 0;
+    end
+  
+    always_comb begin : write_logic
+      for ( int i = 0; i < __const__nbytes_at_write_logic; i += 1 )
+        if ( ( !CSB1 ) && ( !WEB1 ) && WBM1[i] ) begin
+          ram_next[A1][i * 8 +: 8] = I1[i * 8 +: 8];
+        end
+        else
+          ram_next[A1][i * 8 +: 8] = ram[A1][i * 8 +: 8];
+    end
+    always_ff @(posedge clk) begin : update_sram
+      q <= dout_next;
+      if ( ( !CSB1 ) && ( !WEB1 ) ) begin
+        for ( int i = 0; i < __const__num_words_at_update_sram; i += 1 )
+          ram[i] <= ram_next[i];
+      end
+    end
+
+  end
+
+
+// Enable the output
+ always_comb begin : comb_logic
+    if ( !OEB1 ) begin
+      O1 = q;
+    end
+    else
+      O1 = 0;
+  end
+
+endmodule
+
+
+module gf14_sram_256x128_m2 (ceny, gweny, ay, weny, q, so, clk, cen, gwen, a, d, wen,
+    stov, ema, emaw, emas, ten, tcen, tgwen, ta, td, twen, si, se, dftrambyp, ret1n);
+
+  output  ceny;
+  output  gweny;
+  output [7:0] ay;
+  output [127:0] weny;
+  output [127:0] q;
+  output [1:0] so;
+  input  clk;
+  input  cen;
+  input  gwen;
+  input [7:0] a;
+  input [127:0] d;
+  input [127:0] wen;
+  input  stov;
+  input [2:0] ema;
+  input [1:0] emaw;
+  input  emas;
+  input  ten;
+  input  tcen;
+  input  tgwen;
+  input [7:0] ta;
+  input [127:0] td;
+  input [127:0] twen;
+  input [1:0] si;
+  input  se;
+  input  dftrambyp;
+  input  ret1n;
+
+// synopsys translate_off 
+
+  localparam logic [31:0] __const__num_words_at_write_logic  = 32'd256;
+  localparam logic [31:0] __const__num_bits_at_write_logic  = 32'd128;
+  localparam logic [31:0] __const__num_words_at_update_sram  = 32'd256;
+  logic [127:0] ram [0:255];
+  logic [127:0] ram_next [0:255];
+  logic [127:0] dout_next;
+  reg [127:0] q; 
+  always_comb begin : read_logic
+    if ( ( !cen ) && gwen ) begin
+      dout_next = ram[a];
+    end
+    else
+      dout_next = 128'd0;
+  end
+
+always_comb begin : write_logic
+    for ( int i = 0; i < __const__num_words_at_write_logic; i += 1 )
+      ram_next[i] = ram[i];
+    for ( int i = 0; i < __const__num_bits_at_write_logic; i += 1 )
+      if ( ( !cen ) && ( !gwen ) && (!wen[i]) ) begin
+        ram_next[a][i] = d[i];
+      end
+  end
+  always_ff @(posedge clk) begin : update_sram
+    q <= dout_next;
+    if ( ( !cen ) && ( !gwen ) ) begin
+      for ( int i = 0; i < __const__num_words_at_update_sram; i += 1 )
+        ram[i] <= ram_next[i];
+    end
+  end
+// synopsys translate_on
+endmodule
+
+
+module gf14_sram_256x24_m4 (ceny, gweny, ay, q, so, clk, cen, gwen, a, d, stov, ema,
+    emaw, emas, ten, tcen, tgwen, ta, td, si, se, dftrambyp, ret1n);
+  output  ceny;
+  output  gweny;
+  output [7:0] ay;
+  output [23:0] q;
+  output [1:0] so;
+  input  clk;
+  input  cen;
+  input  gwen;
+  input [7:0] a;
+  input [23:0] d;
+  input  stov;
+  input [2:0] ema;
+  input [1:0] emaw;
+  input  emas;
+  input  ten;
+  input  tcen;
+  input  tgwen;
+  input [7:0] ta;
+  input [23:0] td;
+  input [1:0] si;
+  input  se;
+  input  dftrambyp;
+  input  ret1n;
+// synopsys translate_off
+  localparam logic [31:0] __const__nbytes_at_write_logic  = 32'd3;
+  localparam logic [31:0] __const__num_words_at_update_sram  = 32'd256;
+  logic [23:0] dout;
+  logic [23:0] dout_next;
+  logic [23:0] ram [0:255];
+  logic [23:0] ram_next [0:255];
+  reg [23:0] q;
+
+
+  always_comb begin : read_logic
+    if ( ( !cen ) && gwen ) begin
+      dout_next = ram[a];
+    end
+    else
+      dout_next = 24'd0;
+  end
+// NO BIT MASK!
+  always_comb begin : write_logic
+    for ( int i = 0; i < __const__nbytes_at_write_logic; i += 1 )
+      if ( ( !cen) && ( !gwen ) ) begin
+        ram_next[a][i * 8 +: 8] = d[i * 8 +: 8];
+      end
+      else
+        ram_next[a][i * 8 +: 8] = ram[a][i * 8 +: 8];
+  end
+
+  always_ff @(posedge clk) begin : update_sram
+    q <= dout_next;
+    if ( ( !cen ) && ( !gwen ) ) begin
+      for ( int i = 0; i < __const__num_words_at_update_sram; i += 1 )
+        ram[i] <= ram_next[i];
+    end
+  end
+// synopsys translate_on
+endmodule
+
+module gf14_sram_128x32_m2 (ceny, gweny, ay, weny, q, so, clk, cen, gwen, a, d, wen,
+    stov, ema, emaw, emas, ten, tcen, tgwen, ta, td, twen, si, se, dftrambyp, ret1n);
+
+  output  ceny;
+  output  gweny;
+  output [6:0] ay;
+  output [31:0] weny;
+  output [31:0] q;
+  output [1:0] so;
+  input  clk;
+  input  cen;
+  input  gwen;
+  input [6:0] a;
+  input [31:0] d;
+  input [31:0] wen;
+  input  stov;
+  input [2:0] ema;
+  input [1:0] emaw;
+  input  emas;
+  input  ten;
+  input  tcen;
+  input  tgwen;
+  input [6:0] ta;
+  input [31:0] td;
+  input [31:0] twen;
+  input [1:0] si;
+  input  se;
+  input  dftrambyp;
+  input  ret1n;
+
+
+// synopsys translate_off
+  localparam logic [31:0] __const__num_words_at_write_logic  = 32'd128;
+  localparam logic [31:0] __const__num_bits_at_write_logic  = 32'd32;
+  localparam logic [31:0] __const__num_words_at_update_sram  = 32'd128;
+  logic [31:0] dout;
+  logic [31:0] dout_next;
+  logic [31:0] ram [0:127];
+  logic [31:0] ram_next [0:127];
+  reg [31:0] q;
+
+
+  always_comb begin : read_logic
+    if ( ( !cen ) && gwen ) begin
+      dout_next = ram[a];
+    end
+    else
+      dout_next = 32'd0;
+  end
+
+  always_comb begin : write_logic
+    for ( int i = 0; i < __const__num_words_at_write_logic; i += 1 )
+      ram_next[i] = ram[i];
+    for ( int i = 0; i < __const__num_bits_at_write_logic; i += 1 )
+      if ( ( !cen ) && ( !gwen ) && (!wen[i]) ) begin
+        ram_next[a][i] = d[i];
+      end
+  end
+
+  always_ff @(posedge clk) begin : update_sram
+    q <= dout_next;
+    if ( ( !cen ) && ( !gwen ) ) begin
+      for ( int i = 0; i < __const__num_words_at_update_sram; i += 1 )
+        ram[i] <= ram_next[i];
+    end
+  end
+
+// synopsys translate_on
+endmodule
